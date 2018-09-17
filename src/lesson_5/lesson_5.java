e3d337d899f3f2b26efb48dae899f6bcc2ab4c5f
xirurr <xirurr@gmail.com>
2018-09-17 20:37:11 +0200

    ДЗ5

--- src/lesson_5/lesson_5.java
+++ src/lesson_5/lesson_5.java
@@ -1,11 +1,24 @@
+package lesson_5;
+
 public class lesson_5 {
     public static void main(String[] args) {
         new lesson_5().start();
     }
   private void start(){
-        worker first = new worker();
-        first.showInfo();
+        worker[] cabinet = new worker[5];
+        cabinet[0] = new worker();
+        cabinet[1] = new worker("John","nobody","text@gmail.com","666666",10000f,42);
+        cabinet[2] = new worker("Anot","CO","gogo@mail.ru","666667",999999f,35);
+        cabinet[3] = new worker("Gendalf","LORD","nomail","666668",20000f,99);
+        cabinet[4] = new worker("Boris","worker","test@gmail.com","666669",10000f,11);
+
+        for (int i=0; i<cabinet.length;i++){
+            if (cabinet[i].getAge()>40){
+                cabinet[i].showInfo();
+            }
+        }
     }
+
     class worker{
         public worker() {
             this.fullname = "BasicName";
@@ -29,6 +42,10 @@ public class lesson_5 {
         private String phone;
         private Float salary;
         private int age;
+
+        public int getAge() {
+            return age;
+        }
 
         void showInfo() {
             System.out.println("worker{" +
