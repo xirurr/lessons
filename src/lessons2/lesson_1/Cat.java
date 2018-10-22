c9ae4fc944197e3ab07b0cf7de5838b87936842f
xirurr <xirurr@gmail.com>
2018-10-22 15:49:42 +0200

    ДЗ_2.1правильное

--- src/lessons2/lesson_1/Cat.java
+++ src/lessons2/lesson_1/Cat.java
@@ -2,7 +2,7 @@ package lessons2.lesson_1;
 
 public class Cat extends Animal {
     public Cat(String name) {
-        super("Кот", name, (int) (Math.random() * 200) + 10, (int) (Math.random() * 20) + 2, (int) (Math.random() * 20) + 20);
+        super("Кот", name, (int) (Math.random() * 200) + 10, (int) (Math.random() * 20) + 2, (int) (Math.random() * 20) + 2);
     }
 
 }
