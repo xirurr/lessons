e88691069894912b85857b5dd8d998bb517920d1
xirurr <xirurr@gmail.com>
2018-10-24 00:14:29 +0200

    2-2

--- src/lessons2/lesson_2/Day.java
+++ src/lessons2/lesson_2/Day.java
@@ -1,4 +1,5 @@
 package lessons2.lesson_2;
 
 public enum Day {
+    MON,TUE,WED,THU,FRI,SAT,SUN
 }
--- src/lessons2/lesson_2/enumWork.java
+++ src/lessons2/lesson_2/enumWork.java
@@ -1,4 +1,13 @@
 package lessons2.lesson_2;
 
 public class enumWork {
+    public static void main(String[] args) {
+        System.out.println(hoursOfTheDay(Day.SAT));
+    }
+  static  String hoursOfTheDay(Day toDay){
+        if ((5-toDay.ordinal())*8>0){
+            return String.valueOf((5-toDay.ordinal())*8);
+        }
+        else return "WEEKEND";
+    }
 }
